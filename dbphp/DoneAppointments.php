6d3ca84270230726faebba61a1d43baf8fecda8d
caleb-st <caleb_stdenis@hotmail.com>
2016-04-10 22:37:05 -0400

    Prochaine consulation

---
+++ dbphp/DoneAppointments.php
@@ -0,0 +1,6 @@
+<?php include("Header.php"); ?>
+<?php include("nav.php"); ?>
+<body style="text-align:center">
+<h2>Vous n'avez plus de consultations aujourd'hui.</h2>
+<a class="btn btn-default" href="Dashboard.php">Retour au menu principal</a>    
+</body>
